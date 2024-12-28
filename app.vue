692234c1d7dd5f314f7be97068c18b51adffb8a1
aldyryuza <agungaldi34@gmail.com>
2024-12-28 14:20:27 +0700

    LazyCompoents

--- app.vue
+++ app.vue
@@ -1,9 +1,20 @@
+<script setup lang="ts">
+
+
+const show = ref(false);
+const onClick = () => show.value = !show.value;
+
+</script>
+
 <template>
   <div>
     <MainHeader />
     <div>
       <ButtonSubmitButton />
-      <TestComp />
+      <button @click="onClick">
+        {{ show ? 'Hide' : 'Show' }}
+      </button>
+      <LazyTestComp v-if="show" />
     </div>
     <MainFooter />
   </div>
--- custom_components/TestComp.vue
+++ custom_components/TestComp.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        < <h1>Test Component</h1>
+        <h1>Test Component</h1>
     </div>
 </template>
